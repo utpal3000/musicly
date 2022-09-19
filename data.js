195a937ca77ce83115c187f6f04fe593176b306a
utpal3000 <89702252+utpal3000@users.noreply.github.com>
2022-09-19 12:46:59 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'assets/musics/Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'assets/images/cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'assets/musics/Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'assets/images/cover 2.png'
+    },
+    // +6 more
+]
